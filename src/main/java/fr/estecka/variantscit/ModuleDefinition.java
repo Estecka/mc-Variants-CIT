71d7c1f3d4853ee07023ba2957be471ff01f5db3
Estecka <estecka@galacsys.com>
2024-08-20 16:38:30 +0200

    Prototype Module definition

---
+++ src/main/java/fr/estecka/variantscit/ModuleDefinition.java
@@ -0,0 +1,29 @@
+package fr.estecka.variantscit;
+
+import java.util.Map;
+import net.minecraft.util.Identifier;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record ModuleDefinition(Identifier type, String variantDirectory, Identifier fallbackModel, Map<String,Identifier> specialModels)
+{
+	static public final MapCodec<ModuleDefinition> CODEC = RecordCodecBuilder.<ModuleDefinition>mapCodec(builder->builder
+		.group(
+			Identifier.CODEC.fieldOf("type").forGetter(ModuleDefinition::type),
+			Codec.STRING.validate(ModuleDefinition::ValidatePath).fieldOf("variantsDir").forGetter(ModuleDefinition::variantDirectory),
+			Identifier.CODEC.fieldOf("fallback").orElse(null).forGetter(ModuleDefinition::fallbackModel),
+			Codec.unboundedMap(Codec.STRING, Identifier.CODEC).fieldOf("special").orElse(ImmutableMap.<String,Identifier>of()).forGetter(ModuleDefinition::specialModels)
+		)
+		.apply(builder, ModuleDefinition::new)
+	);
+
+	static public DataResult<String> ValidatePath(String path){
+		if (Identifier.isPathValid(path))
+			return DataResult.success(path);
+		else
+			return DataResult.error(()->"Invalid character in directory name: "+path);
+	}
+}
