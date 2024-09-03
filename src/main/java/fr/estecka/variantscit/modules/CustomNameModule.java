d03afcd5dbc76651c73f9b91b1ab9e1cf602af7d
Estecka <estecka@galacsys.com>
2024-09-03 02:37:28 +0200

    Custom_name caching and basic character conversion.

--- src/main/java/fr/estecka/variantscit/modules/CustomNameModule.java
+++ src/main/java/fr/estecka/variantscit/modules/CustomNameModule.java
@@ -2,9 +2,11 @@ package fr.estecka.variantscit.modules;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import fr.estecka.variantscit.VariantsCitMod;
 import fr.estecka.variantscit.api.ISimpleCitModule;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
@@ -22,8 +24,15 @@ implements ISimpleCitModule
 		.apply(builder, CustomNameModule::new)
 	);
 
+	private final WeakHashMap<Text, Identifier> cachedVariants = new WeakHashMap<>();
+
 	private final boolean caseSensitive;
 	private final Map<String,Identifier> specialNames = new HashMap<>();
+	private final Map<Character,Character> illegalConversions = new HashMap<>();
+	{
+		illegalConversions.put(' ', '_');
+		illegalConversions.put('\'', '-');
+	}
 
 	public CustomNameModule(Boolean caseSensitive, Map<String, Identifier> specialNames){
 		this.caseSensitive = caseSensitive;
@@ -39,11 +48,24 @@ implements ISimpleCitModule
 		if (component == null)
 			return null;
 
-		String name = component.getString();
+		return cachedVariants.computeIfAbsent(component, this::GetVariantFromText);
+	}
+
+	public Identifier GetVariantFromText(Text text){
+		String name = text.getString();
+
 		if (!caseSensitive)
 			name = name.toLowerCase();
 
-		Identifier variant = specialNames.get(name);
-		return (variant != null) ? variant : Identifier.tryParse(name);
+		if (specialNames.containsKey(name)){
+			VariantsCitMod.LOGGER.warn("Cached {}: {} -> {}", cachedVariants.size(), text.getString(), name);
+			return specialNames.get(name);
+		}
+
+		for (var e : illegalConversions.entrySet())
+			name = name.replace(e.getKey(), e.getValue());
+
+		VariantsCitMod.LOGGER.warn("Cached {}: {} -> {}", cachedVariants.size(), text.getString(), name);
+		return Identifier.tryParse(name);
 	}
 }
