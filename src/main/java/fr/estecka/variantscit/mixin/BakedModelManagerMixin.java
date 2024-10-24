c0b535eedb4966872a2a7173325da006a78bb952
Estecka <estecka@galacsys.com>
2024-10-24 20:18:50 +0200

    Fixed bad model generation

--- src/main/java/fr/estecka/variantscit/mixin/BakedModelManagerMixin.java
+++ src/main/java/fr/estecka/variantscit/mixin/BakedModelManagerMixin.java
@@ -3,27 +3,24 @@ package fr.estecka.variantscit.mixin;
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import fr.estecka.variantscit.VariantsCitMod;
-import net.minecraft.client.render.model.ModelBaker;
+import net.minecraft.client.render.model.BakedModelManager;
+import net.minecraft.client.render.model.BlockStatesLoader;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 
-@Mixin(ModelBaker.class)
-public class ModelBakerMixin
+@Mixin(BakedModelManager.class)
+public class BakedModelManagerMixin
 {
-	@Shadow private @Final @Mutable Map<Identifier, UnbakedModel> resolvedModels;
-	@Shadow private @Final @Mutable Map<ModelIdentifier, UnbakedModel> models;
-
 	/**
 	 * I could probably use the constructor for JsonUnbakedModel instead, but it
 	 * is unclear how parent-child inheritance works with it.
@@ -38,22 +35,23 @@ public class ModelBakerMixin
 	""";
 
 	@Unique
-	private void AddFromTexture(ModelIdentifier modelId, Identifier parent) {
+	private UnbakedModel CreateFromTexture(ModelIdentifier modelId, Identifier parent) {
 		StringReader reader = new StringReader(ARBITRARY_MODEL.formatted(parent.toString(), modelId.id().toString()));
 		JsonUnbakedModel model = JsonUnbakedModel.deserialize(reader);
-		this.resolvedModels.put(modelId.id(), model);
-		this.models.put(modelId, model);
+		return model;
 	}
 
-	@Inject( method="<init>", at=@At("TAIL") )
-	private void AddVariantModels(Map<?,?> _0, Map<?,?> _1, UnbakedModel _2, CallbackInfo ci) {
+
+	@Inject( method="collect", at=@At("HEAD"))
+	private void AddVariantModels(UnbakedModel missingModel, Map<Identifier, UnbakedModel> inputs, BlockStatesLoader.BlockStateDefinition definition, CallbackInfoReturnable<?> ci, @Local(argsOnly=true) LocalRef<Map<Identifier, UnbakedModel>> inputRef)
+	{
 		// Make mutable
-		this.resolvedModels = new HashMap<>(this.resolvedModels);
-		this.models = new HashMap<>(this.models);
+		inputs = new HashMap<>(inputs);
+		inputRef.set(inputs);
 
 		var models = VariantsCitMod.GetModelsToCreate();
 		VariantsCitMod.LOGGER.info("Creating {} item models from textures...", models.size());
 		for (var entry : VariantsCitMod.GetModelsToCreate().entrySet())
-			this.AddFromTexture(entry.getKey(), entry.getValue());
+			inputs.put(entry.getKey().id(), this.CreateFromTexture(entry.getKey(), entry.getValue()));
 	}
 }
