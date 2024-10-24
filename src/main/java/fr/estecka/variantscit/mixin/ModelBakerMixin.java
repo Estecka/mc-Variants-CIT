a569344cc3ff6673af298d34fea494719335bd7b
Estecka <estecka@galacsys.com>
2024-10-24 17:44:07 +0200

    Changed ModelBaker injection point

--- src/main/java/fr/estecka/variantscit/mixin/ModelBakerMixin.java
+++ src/main/java/fr/estecka/variantscit/mixin/ModelBakerMixin.java
@@ -1,29 +1,33 @@
 package fr.estecka.variantscit.mixin;
 
 import java.io.StringReader;
+import java.util.HashMap;
 import java.util.Map;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fr.estecka.variantscit.VariantsCitMod;
-import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.render.model.ModelBaker;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.profiler.Profiler;
 
 @Mixin(ModelBaker.class)
 public class ModelBakerMixin
 {
-	@Shadow private @Final Map<Identifier, UnbakedModel> resolvedModels;
-	@Shadow private @Final Map<ModelIdentifier, UnbakedModel> models;
+	@Shadow private @Final @Mutable Map<Identifier, UnbakedModel> resolvedModels;
+	@Shadow private @Final @Mutable Map<ModelIdentifier, UnbakedModel> models;
 
+	/**
+	 * I could probably use the constructor for JsonUnbakedModel instead, but it
+	 * is unclear how parent-child inheritance works with it.
+	 */
 	static private final String ARBITRARY_MODEL = """
 		{
 			"parent": "%s",
@@ -41,13 +45,12 @@ public class ModelBakerMixin
 		this.models.put(modelId, model);
 	}
 
-	/**
-	 * Injected right before the post-processing of `modelsToBake`.
-	 */
-	@Inject( method="<init>", at=@At(value="INVOKE", target="java/util/Map.values ()Ljava/util/Collection;") )
-	private void	AddVariantModels(BlockColors _0, Profiler profiler, Map<?,?> _2, Map<?,?> _3, CallbackInfo ci)
-	{
-		profiler.swap("variants-cit");
+	@Inject( method="<init>", at=@At("TAIL") )
+	private void AddVariantModels(Map<?,?> _0, Map<?,?> _1, UnbakedModel _2, CallbackInfo ci) {
+		// Make mutable
+		this.resolvedModels = new HashMap<>(this.resolvedModels);
+		this.models = new HashMap<>(this.models);
+
 		var models = VariantsCitMod.GetModelsToCreate();
 		VariantsCitMod.LOGGER.info("Creating {} item models from textures...", models.size());
 		for (var entry : VariantsCitMod.GetModelsToCreate().entrySet())
