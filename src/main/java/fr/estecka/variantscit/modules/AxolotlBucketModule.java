c4e346c26dd3bb93449bfea2be6d729d5b72ed5c
Estecka <estecka@galacsys.com>
2025-03-26 19:17:06 +0100

    Axolotl baby fallback

--- src/main/java/fr/estecka/variantscit/modules/AxolotlBucketModule.java
+++ src/main/java/fr/estecka/variantscit/modules/AxolotlBucketModule.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fr.estecka.variantscit.CodecUtil;
+import fr.estecka.variantscit.api.ICitModule;
 import fr.estecka.variantscit.format.Substitution;
 import fr.estecka.variantscit.format.properties.AxolotlVariantProperty;
 import fr.estecka.variantscit.format.properties.EntityAgeMapProperty;
@@ -13,22 +14,41 @@ import fr.estecka.variantscit.format.properties.IStringProperty;
 public final class AxolotlBucketModule
 {
 	// TODO: Proper getters
-	static public final MapCodec<MultiComponentFormatModule> CODEC = RecordCodecBuilder.mapCodec(builder->
+	static public final MapCodec<ICitModule> CODEC = RecordCodecBuilder.mapCodec(builder->
 		builder.group(
-			Codec.BOOL.fieldOf("debug").orElse(false).forGetter(o->o.debug),
+			Codec.BOOL.fieldOf("debug").orElse(false).forGetter(o->false),
 			CodecUtil.IDENTIFIER_PATH.fieldOf("adultSuffix").orElse("").forGetter(o->""),
-			CodecUtil.IDENTIFIER_PATH.fieldOf("babySuffix").orElse("").forGetter(o->"")
+			CodecUtil.IDENTIFIER_PATH.fieldOf("babySuffix").orElse("_baby").forGetter(o->"")
 		)
 		.apply(builder, AxolotlBucketModule::Create)
 	);
 
-	static private final Substitution format = Substitution.Parse("${variant}${age}").getOrThrow();
+	static private final Substitution agedFormat = Substitution.Parse("${variant}${age}").getOrThrow();
 
-	static public MultiComponentFormatModule Create(boolean debug, String adult, String baby){
+	static private final HashMap<String,IStringProperty> ageInvariantVariables;
+	static {
+		ageInvariantVariables = new HashMap<>();
+		ageInvariantVariables.put("variant", AxolotlVariantProperty.UNIT);
+	}
+
+	static public ICitModule Create(boolean debug, String adult, String baby){
+		ICitModule result = CreateAgeInvariantModule(debug, adult);
+
+		if (!adult.equals(baby))
+			result = new FallbackModule(CreateAgedModule(debug, adult, baby), result);
+
+		return result;
+	}
+
+	static public MultiComponentFormatModule CreateAgedModule(boolean debug, String adult, String baby){
 		var variables = new HashMap<String, IStringProperty>();
 		variables.put("variant", AxolotlVariantProperty.UNIT);
 		variables.put("age", new EntityAgeMapProperty(adult, baby));
 
-		return new MultiComponentFormatModule(debug, format, variables);
+		return new MultiComponentFormatModule(debug, agedFormat, variables);
+	}
+
+	static public MultiComponentFormatModule CreateAgeInvariantModule(boolean debug, String suffix){
+		return new MultiComponentFormatModule(debug, Substitution.Parse("${variant}"+suffix).getOrThrow(), ageInvariantVariables);
 	}
 }
---
+++ src/main/java/fr/estecka/variantscit/modules/FallbackModule.java
@@ -0,0 +1,21 @@
+package fr.estecka.variantscit.modules;
+
+import org.jetbrains.annotations.Nullable;
+import fr.estecka.variantscit.api.ICitModule;
+import fr.estecka.variantscit.api.IVariantManager;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.item.ItemStack;
+
+public record FallbackModule(ICitModule... innerQueue)
+implements ICitModule
+{
+	@Override
+	public @Nullable ModelIdentifier GetItemModel(ItemStack stack, IVariantManager modelProvider) {
+		for (ICitModule m : innerQueue){
+			ModelIdentifier result = m.GetItemModel(stack, modelProvider);
+			if (result != null) return result;
+		}
+
+		return null;
+	}
+}
