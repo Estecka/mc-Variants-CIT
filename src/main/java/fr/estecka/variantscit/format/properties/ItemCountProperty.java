01f161d156dac83f0fadbe413899527be2d24280
Estecka <estecka@galacsys.com>
2025-03-29 16:37:33 +0100

    Added some documentation

--- src/main/java/fr/estecka/variantscit/format/properties/ItemCountProperty.java
+++ src/main/java/fr/estecka/variantscit/format/properties/ItemCountProperty.java
@@ -10,6 +10,7 @@ implements IStringProperty
 		return stack.getCount();
 	}
 
+	// TODO: Find a way to not return null. This is the only property that returns null on valid properties.
 	@Override
 	public Object GetReference(ItemStack stack) {
 		return null;
