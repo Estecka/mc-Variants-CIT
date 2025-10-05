21b3c3014747c4126f628ab61d6e410cf635d77d
Estecka <estecka@galacsys.com>
2025-10-05 18:06:36 +0200

    Prototype AlternativeTransform

--- src/main/java/fr/estecka/variantscit/VCitRegistries.java
+++ src/main/java/fr/estecka/variantscit/VCitRegistries.java
@@ -29,6 +29,7 @@ public final class VCitRegistries
 		TRANSFORMS.RegisterUnit(Identifier.ofVanilla("sanitize_namespace"), IStringTransform.SANITIZE_NAMESPACE);
 		TRANSFORMS.RegisterUnit(Identifier.ofVanilla("sanitize_auto"),      IStringTransform::AutoSanitize);
 		TRANSFORMS.RegisterMap(Identifier.ofVanilla("successive"),          SuccessiveTransform.MAPCODEC);
+		TRANSFORMS.RegisterMap(Identifier.ofVanilla("alternative"),         AlternativeTransform.MAPCODEC);
 		TRANSFORMS.RegisterMap(Identifier.ofVanilla("char_remap"),          CharRemapTransform.MAPCODEC);
 		TRANSFORMS.RegisterMap(Identifier.ofVanilla("remap"),               RemapTransform.MAPCODEC);
 		TRANSFORMS.RegisterMap(Identifier.ofVanilla("regex"),               RegexTransform.MAPCODEC);
