cad9aededc3205c3efae2158d4ca3eeddcbffef3
Estecka <estecka@galacsys.com>
2025-10-10 16:29:11 +0200

    Simplified remap transform.

--- src/main/java/fr/estecka/variantscit/format/transforms/RemapTransform.java
+++ src/main/java/fr/estecka/variantscit/format/transforms/RemapTransform.java
@@ -3,25 +3,18 @@ package fr.estecka.variantscit.format.transforms;
 import java.util.Map;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fr.estecka.variantscit.format.IStringTransform;
 
 public record RemapTransform(Map<String,String> map) 
 implements IStringTransform
 {
-	static public final MapCodec<RemapTransform> MAPCODEC = RecordCodecBuilder.mapCodec(builder ->
-		builder.group(
-			Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf("map").forGetter(RemapTransform::map)
-		)
-		.apply(builder, RemapTransform::new)
-	);
+	static public final MapCodec<RemapTransform> MAPCODEC = Codec.unboundedMap(Codec.STRING, Codec.STRING)
+		.fieldOf("map")
+		.xmap(RemapTransform::new, RemapTransform::map)
+		;
 
 	@Override
 	public String apply(final String original) {
-		String result = map.get(original);
-		if (result != null)
-			return result;
-		else
-			return null;
+		return map.get(original);
 	}
 }
