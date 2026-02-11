0b01476a9572b5df75e2953493959d26b914f264
Estecka <estecka@galacsys.com>
2026-02-11 21:42:01 +0100

    Fixed Neoforge entrypoint

--- neoforge/src/main/java/fr/estecka/variantscit/neoforge/VariantsCitNeoForge.java
+++ neoforge/src/main/java/fr/estecka/variantscit/neoforge/VariantsCitNeoForge.java
@@ -3,7 +3,7 @@ package fr.estecka.variantscit.neoforge;
 import fr.estecka.variantscit.VariantsCitMod;
 import net.neoforged.fml.common.Mod;
 
-@Mod(VariantsCitMod.MODID)
+@Mod("variantscit")
 public final class VariantsCitNeoForge {
 	public VariantsCitNeoForge() {
 		VariantsCitMod.onInitializeClient();
