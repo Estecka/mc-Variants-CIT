5c07c02fb8ed487a3182c2f257b620f2482e3432
Estecka <estecka@galacsys.com>
2026-02-11 18:59:31 +0100

    Haphazardly moved sources

--- common/src/main/java/fr/estecka/variantscit/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package fr.estecka.variantscit;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "variants-cit";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
