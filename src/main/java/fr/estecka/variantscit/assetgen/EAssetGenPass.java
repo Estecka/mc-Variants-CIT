3e9422b0a11ccefb230c842e6d467d3517dfe419
Estecka <estecka@galacsys.com>
2026-05-14 15:45:55 +0200

    Trim assetGen

--- src/main/java/fr/estecka/variantscit/assetgen/EAssetGenPass.java
+++ src/main/java/fr/estecka/variantscit/assetgen/EAssetGenPass.java
@@ -9,6 +9,7 @@ import fr.estecka.variantscit.reload.EAssetType;
 public enum EAssetGenPass
 implements StringRepresentable
 {
+	TRIMS       ("trims_from_textures",      EAssetType.TRIM_TEXTURE,  EAssetType.TRIM_MODEL ),
 	EQUIPMENTS  ("equipments_from_textures", EAssetType.EQUIP_TEXTURE, EAssetType.EQUIPMENT  ),
 	BAKED_MODELS("models_from_textures",     EAssetType.ITEM_TEXTURE,  EAssetType.BAKED_MODEL),
 	ITEM_STATES ("items_from_models",        EAssetType.BAKED_MODEL,   EAssetType.ITEM_STATE ),
